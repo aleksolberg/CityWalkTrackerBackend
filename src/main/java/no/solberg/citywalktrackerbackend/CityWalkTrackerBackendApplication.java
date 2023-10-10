7a75793d5e2a36401f39a63ee98e424731c83ca8
Aleksander <aleks.solberg@gmail.com>
2023-10-10 20:16:55 +0200

    Initial commit

---
+++ src/main/java/no/solberg/citywalktrackerbackend/CityWalkTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package no.solberg.citywalktrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityWalkTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityWalkTrackerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/solberg/citywalktrackerbackend/CityWalkTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package no.solberg.citywalktrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityWalkTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
